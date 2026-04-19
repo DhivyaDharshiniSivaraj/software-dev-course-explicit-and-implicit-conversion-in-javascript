e000c00c5c56dd8e95750c48819b84af25bfe7df
Dhivya Dharshini Sivaraj <dhidhar.90@gmail.com>
2026-04-19 14:29:15 -0500

    Code correction

---
+++ Untitled.js
@@ -0,0 +1,5 @@
+let total = null;
+console.log("Before:", total, typeof total);
+
+let converted = String(total);
+console.log("After:", converted, typeof converted);
--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -1,4 +1,4 @@
-
+console.log("TASK 1");
 let result = Number("5") - 2; // "It ensures numeric subtraction"
 console.log("The result is: " + String(result)); //"It ensures String output"
 
@@ -11,3 +11,20 @@ if (isValid) {
 let age = "25";
 let totalAge = Number(age) + 5; //"Changes string "25" into a numeric value, preventing the "255" error."
 console.log("Total Age: " + String(totalAge)); //"Prints the totalAge in String"
+
+
+console.log("TASK 2");
+console.log("IMPLICIT")
+
+let value; 
+console.log("Before:", value, "and the type is ",typeof value);
+let answer = value + 5; 
+console.log("After:", answer, "and the type is " ,typeof answer); 
+
+console.log("EXPLICIT")
+
+let total = null;
+console.log("Before:", total, typeof total);
+let converted = String(total);
+console.log("After:", converted, typeof converted);
+
